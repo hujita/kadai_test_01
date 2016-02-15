3482071e6d1f9fc5d9c04205202a82820c4c84c5
藤田一穂 <hujita.k@5sg.jp>
2016-02-15 17:43:35 +0900

    SDL2：テンプレートをCommand Line ToolsではなくCocoa Applicationにしてみる。
    プロジェクト作成しただけで何もいじってない。

---
+++ testtest/testtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testtest
+//
+//  Created by hujita on 2016/02/15.
+//  Copyright (c) 2016年 hujita. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ testtest/testtest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  testtest
+//
+//  Created by hujita on 2016/02/15.
+//  Copyright (c) 2016年 hujita. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
