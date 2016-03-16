e41ccfac42ad9c2388ac9ab157e1d8c315d56ecb
藤田一穂 <hujita.k@5sg.jp>
2016-03-16 15:45:12 +0900

    UPDATE：ブロック配置

---
+++ puzzle_test_02/Assets/Scripts/Gem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour {
+
+	public GameObject sphere;
+	public string ballName = "";
+	
+	string[] ballMats = {"ball_01", "ball_02", "ball_03", "ball_04", "ball_05", "ball_06"};
+
+	public void CreateGem() {
+		ballName = ballMats[Random.Range(0, ballMats.Length)];
+		Material ballMat = Resources.Load ("Materials/" + ballName) as Material;
+		sphere.GetComponent<Renderer>().material = ballMat;
+	}
+
+	// Use this for initialization
+	void Start () {
+		CreateGem ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
