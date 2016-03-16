c22ffd8ac46a25646ce1ef4da333bfb347aa8119
藤田一穂 <hujita.k@5sg.jp>
2016-03-16 15:17:22 +0900

    UPDATE：iOS移植のため別ツールのテスト中

---
+++ puzzle_test_02/Assets/Scripts/GemManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemManager : MonoBehaviour {
+
+	public int gridWidth;
+	public int gridHeight;
+	public GameObject gemPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		for (int y = 0; y < gridHeight; y++) {
+			for (int x = 0; x < gridWidth; x++) {
+				GameObject g = Instantiate(gemPrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+				g.transform.parent = gameObject.transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
