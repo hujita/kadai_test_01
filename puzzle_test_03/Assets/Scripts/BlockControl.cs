1e2348351e7ec5ca86562e449c369697b8078eb7
藤田一穂 <hujita.k@5sg.jp>
2016-03-17 15:57:54 +0900

    UPDATE：ブロックを掴む

--- puzzle_test_03/Assets/Scripts/BlockControl.cs
+++ puzzle_test_03/Assets/Scripts/BlockControl.cs
@@ -33,6 +33,19 @@ public class Block {
 		NUM,
 	};
 
+	public enum STEP {
+		NONE = -1,
+		IDLE = 0,
+		GRABBED,
+		RELEASED,
+		SLIDE,
+		VACANT,
+		RESPAWN,
+		FALL,
+		LONG_SLIDE,
+		NUM,
+	};
+
 	public static int BLOCK_NUM_X = 9;
 	public static int BLOCK_NUM_Y = 9;
 }
@@ -42,14 +55,45 @@ public class BlockControl : MonoBehaviour {
 	public BlockRoot block_root = null;
 	public Block.iPosition i_pos;
 
+	public Block.STEP step = Block.STEP.NONE;
+	public Block.STEP next_step = Block.STEP.NONE;
+	private Vector3 position_offset_initial = Vector3.zero;
+	public Vector3 position_offset = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
 		this.setColor (this.color);
+		this.next_step = Block.STEP.IDLE;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 mouse_position;
+		this.block_root.unprojectMousePosition (out mouse_position, Input.mousePosition);
+		Vector2 mouse_position_xy = new Vector2 (mouse_position.x, mouse_position.y);
+
+		while (this.next_step != Block.STEP.NONE) {
+			this.step = this.next_step;
+			this.next_step = Block.STEP.NONE;
+
+			switch(this.step) {
+			case Block.STEP.IDLE:
+				this.position_offset = Vector3.zero;
+				this.transform.localScale = Vector3.one * 1.0f;
+				break;
+			case Block.STEP.GRABBED:
+				this.transform.localScale = Vector3.one * 1.2f;
+				break;
+			case Block.STEP.RELEASED:
+				this.position_offset = Vector3.zero;
+				this.transform.localScale = Vector3.one * 1.0f;
+				break;
+			}
+
+			Vector3 position = BlockRoot.calcBlockPosition(this.i_pos) + this.position_offset;
+
+			this.transform.position = position;
+		}
 	}
 
 	public void setColor(Block.COLOR color) {
@@ -80,4 +124,41 @@ public class BlockControl : MonoBehaviour {
 		this.GetComponent<Renderer>().material.color = color_value;
 	}
 
+	public void beginGrab(){
+		this.next_step = Block.STEP.GRABBED;
+	}
+
+	public void endGrab() {
+		this.next_step = Block.STEP.IDLE;
+	}
+
+	public bool isGrabbable() {
+		bool is_grabbable = false;
+
+		switch (this.step) {
+		case Block.STEP.IDLE:
+			is_grabbable = true;
+			break;
+		}
+		return(is_grabbable);
+	}
+
+	public bool isContainedPosition(Vector2 position) {
+		bool ret = false;
+		Vector3 center = this.transform.position;
+		float h = Block.COLLISION_SIZE / 2.0f;
+
+		do {
+			if (position.x < center.x - h || center.x + h < position.x) {
+				break;
+			}
+			if (position.y < center.y - h || center.y + h < position.y) {
+				break;
+			}
+
+			ret = true;
+		} while(false);
+
+		return(ret);
+	}
 }
--- puzzle_test_03/Assets/Scripts/BlockRoot.cs
+++ puzzle_test_03/Assets/Scripts/BlockRoot.cs
@@ -5,15 +5,45 @@ public class BlockRoot : MonoBehaviour {
 	public GameObject BlockPrefab = null;
 	public BlockControl[,] blocks;
 
+	private GameObject main_camera = null;
+	private BlockControl grabbed_block = null;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		this.main_camera = GameObject.FindGameObjectWithTag ("MainCamera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 mouse_position;
+		this.unprojectMousePosition (out mouse_position, Input.mousePosition);
+		Vector2 mouse_position_xy = new Vector2 (mouse_position.x, mouse_position.y);
+
+		if (this.grabbed_block == null) {
+			//if (!this.is_has_falling_block ()) {
+				if (Input.GetMouseButtonDown (0)) {
+					foreach (BlockControl block in this.blocks) {
+						if (! block.isGrabbable ()) {
+							continue;
+						}
+
+						if (!block.isContainedPosition(mouse_position_xy)) {
+							continue;
+						}
+
+						this.grabbed_block = block;
+						this.grabbed_block.beginGrab ();
+						break;
+					}
+				}
+			//}
+		} else {
+			if (! Input.GetMouseButton (0)) {
+				this.grabbed_block.endGrab ();
+				this.grabbed_block = null;
+			}
+		}
 	}
 
 	// Creating blocks and deploy
@@ -55,4 +85,21 @@ public class BlockRoot : MonoBehaviour {
 		return(position);
 	}
 
+	public bool unprojectMousePosition(out Vector3 world_position, Vector3 mouse_position) {
+		bool ret;
+		Plane plane = new Plane (Vector3.back, new Vector3 (0.0f, 0.0f, -Block.COLLISION_SIZE / 2.0f));
+
+		Ray ray = this.main_camera.GetComponent<Camera> ().ScreenPointToRay (mouse_position);
+		float depth;
+
+		if (plane.Raycast (ray, out depth)) {
+			world_position = ray.origin + ray.direction * depth;
+			ret = true;
+		} else {
+			world_position = Vector3.zero;
+			ret = false;
+		}
+
+		return(ret);
+	}
 }
