e5865ca643c8ffdddbc336fe0a6c96188c720951
藤田一穂 <hujita.k@5sg.jp>
2016-03-17 13:57:21 +0900

    UPDATE：ブロックを生成 & 並べる

---
+++ puzzle_test_03/Assets/Scripts/BlockControl.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block {
+	public static float COLLISION_SIZE = 1.0f;
+
+	public struct iPosition {
+		public int x;
+		public int y;
+	}
+
+	public enum COLOR {
+		NONE = -1,
+		PINK = 0,
+		BLUE,
+		YELLOW,
+		GREEN,
+		MAGENTA,
+		ORANGE,
+		GRAY,
+		NUM,
+		FIRST = PINK,
+		LAST = ORANGE,
+		NORMAL_COLOR_NUM = GRAY,
+	};
+
+	public enum DIR4 {
+		NONE = -1,
+		RIGHT,
+		LEFT,
+		UP,
+		DOWN,
+		NUM,
+	};
+
+	public static int BLOCK_NUM_X = 9;
+	public static int BLOCK_NUM_Y = 9;
+}
+
+public class BlockControl : MonoBehaviour {
+	public Block.COLOR color = (Block.COLOR)0;
+	public BlockRoot block_root = null;
+	public Block.iPosition i_pos;
+
+	// Use this for initialization
+	void Start () {
+		this.setColor (this.color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setColor(Block.COLOR color) {
+		this.color = color;
+		Color color_value;
+
+		switch (this.color) {
+		default:
+		case Block.COLOR.PINK:
+			color_value = new Color(1.0f, 0.5f, 0.5f);
+			break;
+		case Block.COLOR.BLUE:
+			color_value = Color.blue;
+			break;
+		case Block.COLOR.YELLOW:
+			color_value = Color.yellow;
+			break;
+		case Block.COLOR.GREEN:
+			color_value = Color.green;
+			break;
+		case Block.COLOR.MAGENTA:
+			color_value = Color.magenta;
+			break;
+		case Block.COLOR.ORANGE:
+			color_value = new Color(1.0f, 0.46f, 0.0f);
+			break;
+		}
+		this.GetComponent<Renderer>().material.color = color_value;
+	}
+
+}
---
+++ puzzle_test_03/Assets/Scripts/BlockRoot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockRoot : MonoBehaviour {
+	public GameObject BlockPrefab = null;
+	public BlockControl[,] blocks;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Creating blocks and deploy
+	public void initialSetUp() {
+		this.blocks = new BlockControl [Block.BLOCK_NUM_X, Block.BLOCK_NUM_Y];
+
+		int color_index = 0;
+
+		for (int y = 0; y < Block.BLOCK_NUM_Y; y++) {
+			for (int x = 0; x < Block.BLOCK_NUM_X; x++) {
+				GameObject game_object = Instantiate (this.BlockPrefab) as GameObject;
+				BlockControl block = game_object.GetComponent<BlockControl>();
+				this.blocks[x, y] = block;
+
+				block.i_pos.x = x;
+				block.i_pos.y = y;
+
+				block.block_root = this;
+
+				Vector3 position = BlockRoot.calcBlockPosition(block.i_pos);
+				block.transform.position = position;
+				block.setColor ((Block.COLOR)color_index);
+
+				block.name = "block(" + block.i_pos.x.ToString () + "," + block.i_pos.y.ToString() + ")";
+
+				color_index = Random.Range (0, (int)Block.COLOR.NORMAL_COLOR_NUM);
+
+			}
+		}
+	}
+
+	public static Vector3 calcBlockPosition(Block.iPosition i_pos) {
+		Vector3 position = new Vector3 (-(Block.BLOCK_NUM_X / 2.0f - 0.5f),
+		                               -(Block.BLOCK_NUM_Y / 2.0f - 0.5f), 0.0f);
+
+		position.x += (float)i_pos.x * Block.COLLISION_SIZE;
+		position.y += (float)i_pos.y * Block.COLLISION_SIZE;
+
+		return(position);
+	}
+
+}
